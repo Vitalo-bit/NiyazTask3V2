e49b3680470aa10055b72a1cfc3edf72b5e9f238
Niyaz <sueraetae@mail.ru>
2023-06-24 22:20:22 +0300

    1

---
+++ NiyazTask3V22/NiyazTask3V22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiyazTask3V22
+//
+//  Created by Александр Троицкий on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
